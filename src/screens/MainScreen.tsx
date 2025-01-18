bf3429d68fbea84306f2e8c1f48c114d131f7d58
jjiinaaa <qka0103@gmail.com>
2025-01-19 02:19:19 +0900

    [CARE-24] BottomBar UI 구현

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import '../styles/global.css';
+import tw from 'tailwind-react-native-classnames';
 
+import SvgIcon from '../components/SvgIcon';
 import SampleHomeScreen from './SampleHomeScreen';
-import TestScreen from './TestScreen';
+import MyInfoScreen from './MyInfoScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,13 +17,27 @@ function MainScreen() {
         component={SampleHomeScreen}
         options={{
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <SvgIcon name={focused ? 'HomeActive32' : 'HomeDisabled32'} size={32} />
+          ),
+          tabBarStyle: tw`h-16`,
+          tabBarLabelStyle: tw`mt-1 text-sm`,
+          tabBarActiveTintColor: '#234A7C',
+          tabBarInactiveTintColor: '#8E8E8E',
         }}
       />
       <Tab.Screen
         name="내 정보"
-        component={SampleHomeScreen}
+        component={MyInfoScreen}
         options={{
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <SvgIcon name={focused ? 'AccountActive32' : 'AccountDisabled32'} size={32} />
+          ),
+          tabBarStyle: tw`h-16`,
+          tabBarLabelStyle: tw`mt-1 text-sm`,
+          tabBarActiveTintColor: '#234A7C',
+          tabBarInactiveTintColor: '#8E8E8E',
         }}
       />
     </Tab.Navigator>
