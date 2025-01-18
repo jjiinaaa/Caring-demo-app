7f99f660575ba0adcdaa85cfc71c24fabd7fe16e
jjiinaaa <qka0103@gmail.com>
2025-01-19 02:22:13 +0900

    [CARE-24] 내정보 경로 지정

---
+++ src/screens/MyInfoScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function MyInfoScreen(): React.JSX.Element {
+  return (
+    <View>
+      <Text>MyInfo Screen</Text>
+    </View>
+  );
+}
