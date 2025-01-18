539a1ba18d6f5ec15da2559b6b0d0901d088e346
jjiinaaa <qka0103@gmail.com>
2025-01-19 02:59:55 +0900

    [CARE-24] 내정보 페이지 Stack 포함

--- App.tsx
+++ App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import MainScreen from './src/screens/MainScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import TestScreen from './src/screens/TestScreen';
 import SampleHomeScreen from './src/screens/SampleHomeScreen';
+import MyInfoScreen from './src/screens/MyInfoScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,11 +17,6 @@ function App(): React.JSX.Element {
         <Stack.Screen
           name="Main"
           component={MainScreen}
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Test" component={TestScreen} />
-        <Stack.Screen
-          name="SampleHome"
-          component={SampleHomeScreen}
           options={{
             headerShown: false,
           }}
@@ -27,6 +24,7 @@ function App(): React.JSX.Element {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Test" component={TestScreen} />
         <Stack.Screen name="SampleHome" component={SampleHomeScreen} />
+        <Stack.Screen name="MyInfo" component={MyInfoScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
