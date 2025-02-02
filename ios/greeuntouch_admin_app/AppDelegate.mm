cc2bdcaa0c86b07779de786f1c3741c683e784c5
jjiinaaa <qka0103@gmail.com>
2025-02-02 17:05:21 +0900

    [CARE-40] 프로직트 이름 변경 및 tailwind global 적용

--- ios/greeuntouch_admin_app/AppDelegate.mm
+++ ios/greeuntouch_admin_app/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"greeuntouch_admin_app";
+  self.moduleName = @"greentouch_admin_app";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
